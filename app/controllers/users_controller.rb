49f119c3d802f8c0a05d5aeb601b77b5ec1bca4b
yudai-simoma <cdcdasdll@icloud.com>
2021-03-24 16:52:14 +0900

    エラーの修正（ユーザー情報・編集ページリンク・ログイン状態でのアクション）

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,6 @@ class UsersController < ApplicationController
   #ユーザーの詳細情報へ遷移
   def show
     @user = User.find(params[:id])
-    @prototypes = Prototype.all
+    @prototypes = @user.prototypes
   end
 end
